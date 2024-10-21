741c849a41a6ff7543750945076c6d14b56595c3
MaxDanna26 <maximilianodanna@hotmail.com>
2024-10-21 20:35:24 +0200

    20:35

--- Array/Vector.cpp
+++ Array/Vector.cpp
@@ -1,11 +1,37 @@
 #include "Vector.h"
 #include <iostream>
+#include <algorithm>
 
 Vector::Vector(int n)
 {
 	this->n = n;
 	this->pos = 0;
 	this->ptr = new int(n);
+}
+
+Vector::Vector(const Vector &v)
+{
+	//Reservar el mismo tamaño que tenga v
+	this->ptr = new int[v.n];
+	
+	//Copiar los datos del puntero ptr a this
+	std::copy(this->ptr, v.ptr+v.pos, this->ptr);
+	
+	//Copiar el resto de atributos
+	this->n = v.n;
+	this->pos = v.pos;
+}
+
+Vector & Vector::operator=(const Vector &v)
+{
+	//Liberar la memoria ocupada en this
+	delete [] this->ptr;
+	
+	//Copiar los datos 
+	
+	
+	//Devolver una ref a this
+	return *this;
 }
 
 bool Vector::push(int item)
@@ -21,6 +47,8 @@ bool Vector::push(int item)
 		return false;
 	}
 }
+
+
 
 void Vector::print(){
 	std::cout << "[";
--- Array/Vector.h
+++ Array/Vector.h
@@ -10,6 +10,14 @@ class Vector
 	
 	public:
 		Vector(int=10);
+		
+		// Si no queremos utilizar el constructor copia y operador = lo borramos
+		//Vector(const Vector &)=delete) No sirve porque no deja hacer copia
+		//Vector & operator=(const Vector &)=delete;
+		
+		Vector(const Vector &);
+		Vector & operator=(const Vector &);
+		
 		bool push(int);
 		int lenght(){return this->pos;}
 		int capacity(){return this->n;}
